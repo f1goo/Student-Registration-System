faf3df724f2b80a9af2e0d219de09eb5c48fe1b6
Figo <Figopinto@hotmail.com>
2025-10-22 13:00:19 +0100

    Session 4 updates

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 from user import User
+from student import Student
 from database import Database
 from getpass import getpass
 
 db = Database()
 user = User(db)
+student = Student(db)
 
 while True:
-    print("\n1. Register\n2. Login\n3. Exit")
+    print("\n1. Register\n2. Login\n3. Add Student\n4.Exit")
     choice = input("Select option: ")
 
     if choice == '1':
@@ -22,7 +24,14 @@ while True:
         else:
             print("Invalid credentials")
 
-    elif choice == '3':
+    elif choice =='3':
+        name = input("Enter name: ")
+        dob = input("Enter DOB (dd-mm-yyyy): ")
+        course = input("Enter course: ")
+        email = input("Enter email: ")
+        student.add_student(name,dob,course,email)
+
+    elif choice == '4':
         break
     else:
         print("Invalid input.")
---
+++ student.py
@@ -0,0 +1,13 @@
+from user import User
+from database import Database
+
+class Student:
+    def __init__(self,db):
+        self.db = db 
+
+    def add_student(self,name,dob,course,email):
+        self.db.execute("INSERT INTO student (name,dob,course,email) VALUES (%s, %s, %s, %s)",
+            (name,dob,course,email),
+            commit = True
+        )
+        print(f"Student {name} has been added sucessfully")
