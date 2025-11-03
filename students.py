33a49a3cf3e654ba20b913338bb8e10d274f4f55
Figo <Figopinto@hotmail.com>
2025-11-03 17:58:10 +0000

    Session 6 Updates

--- students.py
+++ students.py
@@ -1,15 +1,59 @@
-from user import User
+from users import Users
 from database import Database
 
-class Student:
+class Students:
     def __init__(self,db):
         self.db = db 
 
     def add_student(self,name,dob,course,email):
-        self.db.execute("INSERT INTO student (name,dob,course,email) VALUES (%s, %s, %s, %s)",
+        self.db.execute("INSERT INTO students (name,dob,course,email) VALUES (%s, %s, %s, %s)",
             (name,dob,course,email),
             commit = True
         )
         print(f"Student {name} has been added sucessfully")
 
-    def update_student()
+    def get_student_ID(self,student_id):
+        result = self.db.execute(
+            "SELECT * FROM users WHERE username=%s AND password=%s",
+            (student_id,)
+        ).fetchone()
+        if result:
+            print(f"Student found",result)
+            return True
+        else:
+            print(f"No student with the student id {student_id}")
+            return False
+
+    def update_student_details(self, student_id, dob=None, course=None, email=None):
+        query = "UPDATE students SET"
+        values = []
+        updates = []
+
+        if dob:
+            updates.append("dob = %s")
+            values.append(dob)
+        if course:
+            updates.append("course = %s")
+            values.append(course)
+        if email:
+            updates.append("email = %s")
+            values.append(email)
+        
+        if not updates:
+            print("No fields to update.")
+            return
+        query = f"UPDATE student SET { ",".join(updates)} WHERE student_id = %s"
+        values.append(student_id)
+
+        self.db.execute(query, tuple(values),commit=True)
+        print(f"Student {student_id} updated successfully!")
+
+    def delete_student(self, student_id):
+        result = self.db.execute(
+            "DELETE FROM students WHERE student_id=%s",
+            (student_id,)
+        )
+        if result:
+            print(f"Student {student_id} deleted successfully", result)
+        else:
+            print(f"No student found with the student id: {student_id}")
--- users.py
+++ users.py
@@ -5,18 +5,18 @@ import mysql.connector
 db = Database()
 
 
-class User:
+class Users:
     def __init__(self, db):
         self.db = db
 
     def register(self, username, password):
         hashed = hashlib.sha256(password.encode()).hexdigest()
-        result = self.db.execute("SELECT * FROM user WHERE username=%s", (username,)).fetchone()
+        result = self.db.execute("SELECT * FROM users WHERE username=%s", (username,)).fetchone()
         if result:
             print("Username already exists")
         else:
             self.db.execute(
-                "INSERT INTO user (username, password) VALUES (%s, %s)",
+                "INSERT INTO users(username, password) VALUES (%s, %s)",
                 (username, hashed),
                 commit=True
             )
@@ -24,7 +24,7 @@ class User:
 
     def login(self, username, password):
         hashed = hashlib.sha256(password.encode()).hexdigest()
-        result = self.db.execute("SELECT * FROM user WHERE username=%s", (username,)).fetchone()
+        result = self.db.execute("SELECT * FROM users WHERE username=%s", (username,)).fetchone()
 
         if result and result["password"] == hashed:
             return True
